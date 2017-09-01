26a9fcbc54906e3c30fe51ee327b3cd1166fecfd
Holt Mansfield <HoltMansfield@gmail.com>
2017-08-31 20:01:31 -0400

    first crack at menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
 import { Rating } from 'semantic-ui-react'
+import { Dropdown, Icon, Input, Menu, Segment } from 'semantic-ui-react'
 import './App.css'
 import logo from './style/images/stanford.jpeg'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.activeItem = ''
+  }
+
   render() {
     return (
       <div>
-        <div className="ui fixed menu">
-          <a><img src={logo} height="80" /></a>
-          <a className="item"></a>
-          <a className="item">Editorials</a>
-          <a className="item">Reviews</a>
-          <a className="item">Upcoming Events</a>
-        </div>
-        <div style={{ height: '1900px' }}>mama</div>
+        <Menu attached='top'>
+          <Menu.Menu>
+            <Dropdown item icon='content' simple>
+              <Dropdown.Menu>
+                <Dropdown.Item>Open</Dropdown.Item>
+                <Dropdown.Item>Save...</Dropdown.Item>
+                <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Export</Dropdown.Header>
+                <Dropdown.Item>Share</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Menu>
+          <img src={logo} height="70" />
+          <Menu.Menu position='right'>
+            <Dropdown item icon='wrench' simple>
+              <Dropdown.Menu className='left'>
+                <Dropdown.Item>Open</Dropdown.Item>
+                <Dropdown.Item>Save...</Dropdown.Item>
+                <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Export</Dropdown.Header>
+                <Dropdown.Item>Share</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Menu>
+        </Menu>
+
+        <Segment attached='bottom'>
+          <img src='/assets/images/wireframe/paragraph.png' />
+        </Segment>
       </div>
     );
   }
