e4f89a1342814e852bca8b20fe31591e761203b6
Holt Mansfield <HoltMansfield@gmail.com>
2017-08-31 22:23:32 -0400

    parking

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,45 @@
 import React, { Component } from 'react'
 import { Rating } from 'semantic-ui-react'
-import { Dropdown, Icon, Input, Menu, Segment } from 'semantic-ui-react'
+import { Dropdown, Icon, Input, Menu, Header, Image, Sidebar, Segment } from 'semantic-ui-react'
 import './App.css'
 import logo from '../../style/images/stanford.jpeg'
 
 class App extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   constructor(props) {
     super(props)
     this.activeItem = ''
   }
 
   render() {
+    const { visible } = this.state
+
     return (
       <div>
         <Menu attached='top'>
-          <Menu.Menu>
-            <Dropdown item icon='content' simple>
-              <Dropdown.Menu>
-                <Dropdown.Item>Open</Dropdown.Item>
-                <Dropdown.Item>Save...</Dropdown.Item>
-                <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Header>Export</Dropdown.Header>
-                <Dropdown.Item>Share</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu.Menu>
+          <Menu.Item
+            icon="content"
+            onClick={this.toggleVisibility}>
+          </Menu.Item>
           <img src={logo} height="70" />
-          <Menu.Menu position='right'>
-            <Dropdown item icon='wrench' simple>
-              <Dropdown.Menu className='left'>
-                <Dropdown.Item>Open</Dropdown.Item>
-                <Dropdown.Item>Save...</Dropdown.Item>
-                <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Header>Export</Dropdown.Header>
-                <Dropdown.Item>Share</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu.Menu>
         </Menu>
+        <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' onMouseLeave={this.toggleVisibility} vertical inverted>
+          <Menu.Item name='home'>
+            <Icon name='home' onClick={this.toggleVisibility} />
+            Home
+          </Menu.Item>
+          <Menu.Item name='gamepad'>
+            <Icon name='gamepad' />
+            Games
+          </Menu.Item>
+          <Menu.Item name='camera'>
+            <Icon name='camera' />
+            Channels
+          </Menu.Item>
+        </Sidebar>
       </div>
     );
   }
