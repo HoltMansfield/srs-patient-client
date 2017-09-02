cf4c57a9d295118da954a052ea2b4c13b2cec9d5
Holt Mansfield <HoltMansfield@gmail.com>
2017-09-01 22:08:51 -0400

    login component

--- src/components/users/login/Login.js
+++ src/components/users/login/Login.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
+import { Segment, Grid, Form, Button } from 'semantic-ui-react'
 
 import * as actions from '../../../actions'
 
 export class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit() {
+    const newUser = {
+      email: ReactDOM.findDOMNode(this.refs.email).value,
+      password: ReactDOM.findDOMNode(this.refs.password).value
+    }
+
+    console.log(newUser)
+  }
+
   render() {
     return (
-      <div>Loginly</div>
+      <Grid padded>
+        <Grid.Row columns={3}>
+          <Grid.Column></Grid.Column>
+          <Grid.Column>
+            <Segment raised>
+              <Form>
+              <Form.Field>
+                <label>E-Mail</label>
+                <input placeholder='email' ref='email' />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input placeholder='password' ref='password' />
+              </Form.Field>
+              <Button type='submit' onClick={this.handleSubmit}>Create Account</Button>
+            </Form>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column></Grid.Column>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
