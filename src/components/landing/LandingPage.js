1fb8237c8f17d466d466fc779888f36096534445
Holt Mansfield <HoltMansfield@gmail.com>
2017-09-01 20:18:15 -0400

    tweaks

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -7,19 +7,21 @@ export class LandingPage extends Component {
     return (
       <div className="padded-page">
         <Grid textAlign='center' divided='vertically'>
-          <Grid.Row columns={2}>
+          <Grid.Row columns={4}>
+            <Grid.Column></Grid.Column>
             <Grid.Column>
               <Link to="/create-account" title="Click here to create an account">
-                <div><Icon circular name='user' size='massive' color='grey' /></div>
+                <div><Icon circular name='add user' size='massive' color='grey' /></div>
                 <div className="margin-top-10"><Button>Create new account</Button></div>
               </Link>
             </Grid.Column>
             <Grid.Column>
               <Link to="/login" title="Click here to login">
-                <div><Icon circular name='add user' size='massive' color='grey' /></div>
+                <div><Icon circular name='user' size='massive' color='grey' /></div>
                 <div className="margin-top-10"><Button>Login</Button></div>
               </Link>
             </Grid.Column>
+            <Grid.Column></Grid.Column>
           </Grid.Row>
         </Grid>
       </div>
