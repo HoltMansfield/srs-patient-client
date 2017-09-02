df21077dfc8a23f2981b3e26e5b7c19f6833e9ec
Holt Mansfield <HoltMansfield@gmail.com>
2017-09-01 21:43:58 -0400

    switched to segements

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
-import { Grid, Icon, Button } from 'semantic-ui-react'
+import { Grid, Icon, Button, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 export class LandingPage extends Component {
   render() {
     return (
-      <div className="padded-page">
+      <div className='padded-page'>
         <Grid textAlign='center' divided='vertically'>
           <Grid.Row columns={4}>
             <Grid.Column></Grid.Column>
             <Grid.Column>
-              <Link to="/create-account" title="Click here to create an account">
-                <div><Icon circular name='add user' size='massive' color='grey' /></div>
-                <div className="margin-top-10"><Button>Create new account</Button></div>
+              <Link to='/create-account' title='Click here to create an account'>
+                <Segment attached='top' padded='very'>
+                  <Icon circular name='add user' size='big' color='grey' />
+                </Segment>
+                <Button
+                  attached='bottom'
+                  content='Create new account'
+                />
               </Link>
             </Grid.Column>
             <Grid.Column>
-              <Link to="/login" title="Click here to login">
-                <div><Icon circular name='user' size='massive' color='grey' /></div>
-                <div className="margin-top-10"><Button>Login</Button></div>
+              <Link to='/login' title='Click here to login'>
+                <Segment attached='top' padded='very'>
+                  <Icon circular name='user' size='big' color='grey' />
+                </Segment>
+                <Button
+                  attached='bottom'
+                  content='Login'
+                />
               </Link>
             </Grid.Column>
             <Grid.Column></Grid.Column>
