db7f14cde6b0f67680ce2b3fb67a963802c2a928
Holt Mansfield <HoltMansfield@gmail.com>
2017-12-08 17:44:52 -0500

    rando

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import Notifications from 'react-notify-toast'
-
+import { Swipeable, defineSwipe } from 'react-touch'
 import Routes from '../../components/routing/Routes'
 import Overlay from '../../components/overlay/Overlay'
 
 export class App extends Component {
+  swipeRight() {
+    console.log('not rocket appliances')
+  }
+  
   render() {
+    const swipe = defineSwipe({swipeDistance: 5});
+
     return (
       <div>
         <Notifications />
-        <Routes />
+          <Swipeable config={swipe} onSwipeRight={this.swipeRight}>
+            <Routes />
+          </Swipeable>
+
         <Overlay />
       </div>
     )
--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
-//import { Grid, Icon, Button, Segment } from 'semantic-ui-react'
+import { Swipeable, defineSwipe } from 'react-touch'
 import { Link } from 'react-router-dom'
 import { DatePicker } from 'antd'
 
 
 
 export class LandingPage extends Component {
+  swipeRight() {
+    console.log('not rocket appliances')
+  }
+
   render() {
+    const swipe = defineSwipe({swipeDistance: 5});
+
     return (
       <div className='padded-page'>
-landing
-<DatePicker />
+      Landing page
+      <Swipeable config={swipe} onSwipeRight={this.swipeRight}>
+        <div>MAMA</div>
+      </Swipeable>
+      <DatePicker />
       </div>
     )
   }
