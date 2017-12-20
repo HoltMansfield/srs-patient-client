8a31e34e82f9b0deff262610c03b27bad90699d5
Holt Mansfield <HoltMansfield@gmail.com>
2017-12-20 18:57:13 -0500

    sidey guy implemented

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,64 @@
 import React, { Component } from 'react'
 import Notifications from 'react-notify-toast'
 import { Layout, Menu, Icon } from 'antd'
+import Sidebar from 'react-sidebar';
 import Routes from '../../components/routing/Routes'
 import Overlay from '../../components/overlay/Overlay'
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sidebarOpen: false
+    }
+
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({sidebarOpen: open});
+  }
+
   render() {
-    const { Header, Content, Footer } = Layout;
+    const { Header, Content, Footer } = Layout
+    const sidebarContent = <div><b>Sidebar content</b></div>
+    const sideStyles = {
+      overlay: {
+        zIndex: 1,
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        visibility: 'hidden',
+        transition: 'opacity .3s ease-out, visibility .3s ease-out',
+        backgroundColor: 'rgba(0,0,0,.3)',
+      }
+    }
 
     return (
       <div>
-        <Notifications />
-          <Layout>
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0 }} />
-              <Content style={{ margin: '24px 16px 0' }}>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                  <Routes />
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>
-                Ant Design ©2016 Created by Ant UED
-              </Footer>
-            </Layout>
-          </Layout>
-        <Overlay />
+        <Sidebar sidebar={sidebarContent} style={sideStyles}
+         open={this.state.sidebarOpen}
+         onSetOpen={this.onSetSidebarOpen}>
+         <Notifications />
+           <Layout>
+             <Layout>
+               <Header style={{ background: '#fff', padding: 0 }} />
+               <Content style={{ margin: '24px 16px 0' }}>
+                 <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                   <Routes />
+                 </div>
+               </Content>
+               <Footer style={{ textAlign: 'center' }}>
+                 Ant Design ©2016 Created by Ant UED
+               </Footer>
+             </Layout>
+           </Layout>
+         <Overlay />
+       </Sidebar>
       </div>
     )
   }
